3770f08f8ac4cef7807d6f7d1a5b717387270432
lukaivancic <lukaivancic989@gmail.com>
2021-04-11 01:37:12 +0200

    Initial commit  #2

---
+++ src/main/java/util/Utils.java
@@ -0,0 +1,23 @@
+package util;
+
+import io.restassured.response.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Utils { //glavni setup za restassured
+
+	//metode za rest assured za setup, da wrepujem
+	public static String TESTING_EMAIL = "humantesting69";
+	public static String TESTING_EMAIL_DOMAIN = "@gmail.com";
+
+	public static String emailBuilder(){
+
+		String generatedstring= RandomStringUtils.randomAlphabetic(8);
+
+		return TESTING_EMAIL + "+" + generatedstring + TESTING_EMAIL_DOMAIN;
+
+
+	}
+
+
+
+}
